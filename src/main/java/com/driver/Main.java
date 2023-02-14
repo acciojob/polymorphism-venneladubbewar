8270a5c3f92816841b52076ac8d6da45050940d1
kiran <Kiran.dubbewar18@users.noreply.github.com>
2023-02-14 11:10:55 +0530

    second

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,33 @@
 package com.driver;
 
-public class Main {
+import java.util.Scanner;
 
+public class Main {
+    public static class Product {
+        public int product(int x, int y) {
+           return  x*y;
+        }
+
+        public int product(int x, int y, int z) {
+             return x*y*z;
+        }
+
+        public double product(double x, double y) {
+          return x*y;
+        }
+    }
+
+
+    public static void main(String[] args) {
+
+    Product p = new Product();
+    p.product(5,10);
+    p.product(5,6,2);
+    p.product(5.0,6.0);
+
+
+
+
+
+    }
 }
